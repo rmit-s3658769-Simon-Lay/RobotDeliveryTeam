3fd49eab8e1915a07aad7c7fd2cdf4ab95b79655
Simon Lay <s3658769@student.rmit.edu.au>
2020-03-24 16:28:02 +1100

    added scp file transfer capability. However normal git commands still need to be run. This is to ease the process of transferring files

---
+++ version-control.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#This program will use your public key to ssh into the robodelivery server and pull
+#everything (including this script) to your local machine.  
+#
+#Where to place this script?
+#Please place this script in the directory where you have the project you want to 
+#upload to github.
+#example:
+# ls
+# baxter-sim-demo/
+#
+
+############################     NOTE    ###############################
+#		This is only used for file transfer
+#		You would still need to
+#		git commands to push to the server
+#
+#######################################################################
+
+####### Parameters #########
+privatekey=$1
+location_in_server=//home/robodeliver/GithubFile
+
+# shh into the robodeliver server
+scp -r -i $privatekey robodeliver@131.170.250.237:$location_in_server .
+
