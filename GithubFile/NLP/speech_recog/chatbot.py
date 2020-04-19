ec1f669863f04209e289ddbcc4106b0acf4423f0
Simon Lay <s3658769@student.rmit.edu.au>
2020-04-19 19:48:47 +1000

    created audio scriptsand chatbot changes

--- GithubFile/NLP/speech_recog/chatbot.py
+++ GithubFile/NLP/speech_recog/chatbot.py
@@ -2,23 +2,13 @@
 import io
 import random
 import string # to process standard python strings
-import warnings
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import warnings
-warnings.filterwarnings('ignore')
-
 import nltk
 from nltk.stem import WordNetLemmatizer
-#Usage goal:
-#Use the livespeech library
-
+from nltk.corpus import wordnet
 from pocketsphinx import LiveSpeech
-#for phrase in LiveSpeech():
-#    print(phrase)
-#    if str(phrase) ==  "go forward":
-#        print("okay")
 
 nltk.download('popular', quiet=True) # for downloading packages
 # uncomment the following only the first time
@@ -27,9 +17,11 @@ nltk.download('popular', quiet=True) # for downloading packages
 #Reading in the corpus
 with open('chatbot-corpus.txt','r') as fin:
     raw = fin.read().lower()
-#TOkenisation
+
+#Tokenisation
 sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
 word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
 # Preprocessing
 lemmer = WordNetLemmatizer()
 def LemTokens(tokens):
@@ -39,19 +31,61 @@ def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 # Keyword Matching
-#Import a list of responses from a txt file and parse then
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+#Do greetings via wordnet
+#def Check_wordnet(word):
+#    for synonym in wordnet.synsets(word):
+#        for lemma in synonym.lemmas():
+            
+        
 
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
+    return_array = []
+    for synonym in wordnet.synsets(sentence):
+        for lemma in synonym.lemmas():
+            return_array.append(lemma.name())
+    #return a random string
+    return random.choice(return_array)
+
+def baxter_commands(user_response): 
+    basic_commands_array = ['forward', 'stop', 'slow', 'spin', 'turn', 'execute']
+    commands = user_response.split(" ")
+    run_cmd = '*rospy cmd*'
+    run_flag = False
+    print(commands)
+    for i in commands:
+        if run_flag == False:
+            if user_response not in basic_commands_array:
+                return False
+            if ('forward' == i):
+                print('moving forward....')
+                ##insert rospy command here
+            if ('stop' == i):
+                print('stopping.......')
+                ##insert rospy command here
+            if ('slow' == i):
+                print('slowing down.....')
+                ##insert rospy command here
+            if ('spin' == i):
+                print("spinning........")
+                ##insert rospy command here
+            if ('turn' == i):
+                print("turning.....")
+                ##insert rospy command here
+            if ('execute' == i):
+                run_flag = True
+        else:
+            if i not in basic_commands_array:
+                run_cmd = run_cmd+" "+i
+            else:
+                print("execute the rospy command: "+run_cmd)
+                ##insert rospy command here
+
+
 
 
 # Generating response
-def response(user_response):
+def Use_Corpus(user_response):
     robo_response=''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
@@ -73,25 +107,25 @@ print("Rosie: If you want to exit, say Bye!")
 for phrase in LiveSpeech():
     print('you said:')
     print(phrase)
-#    if str(phrase) ==  "go forward":
-#        print("okay")
     
     user_response = str(phrase)
     user_response = user_response.lower()
+
     #Insert Baxter bot commands#
-    if (user_response == 'go forward'):
-        print('moving forward....')
-    elif(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print("Rosie: You are welcome..")
-        else:
-            if(greeting(user_response)!=None):
-                print("Rosie: "+greeting(user_response))
+    if baxter_commands(user_response) == False:
+        ##Conversational
+        if(user_response!='bye'):
+            analyse = wordnet.synsets(user_response)
+            if isinstance(analyse,list) == True and not analyse == []:
+                definition = analyse[0].definition()
+                print(definition)
+                if ('greeting' in definition):
+                    print("Rosie: "+greeting(user_response))
             else:
-                print("Rosie:\n")
-                print(response(user_response))
+                print("Rosie:")
+                print(Use_Corpus(user_response))
                 sent_tokens.remove(user_response)
-    else:
-        print("Rosie: Bye! take care..")  
-        break
+        else:
+            print("Rosie: Bye! take care..")  
+            break
+
