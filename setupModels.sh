ace4146a7356cb3b802ea907025acadc6ef4d523
samcdc6600 <ntpower7000@gmail.com>
2020-04-26 14:03:04 +0000

    We have fixed an issued with the loading of the beer can holder model in .world file

--- setupModels.sh
+++ setupModels.sh
@@ -3,3 +3,5 @@
 #!/bin/sh
 cp -r gazeboModels/* /root/ws_baxter/src/baxter_simulator/baxter_sim_examples/models/
 cp vxlab.world /root/
+git config --global user.email "ntpower7000@gmail.com"
+git config --global user.name "samcdc6600"
