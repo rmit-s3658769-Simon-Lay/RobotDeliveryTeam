caa92c369a4da168b48605aa8f0527daa33d6bf3
Simon Lay <s3658769@student.rmit.edu.au>
2020-05-04 12:52:01 +1000

    made modifications to vel.py velocity works but get's stuck in a whileloop

--- GithubFile/NLP/speech_recog/vel.py
+++ GithubFile/NLP/speech_recog/vel.py
@@ -9,10 +9,10 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 def motion_demo():
     velocity_publisher = rospy.Publisher('/mobility_base/cmd_vel', Twist, queue_size=1)
-    rospy.init_node('motion_demo');
-
+    rospy.init_node('motion_demo_py')
+    print("setting constants")
     vel_msg = Twist()
-    speed = 10
+    speed = 0.1
     distance = 10
 
     #move forward
@@ -22,18 +22,21 @@ def motion_demo():
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
-    
+    print("movement forward has commenced")
     while not rospy.is_shutdown():
 	t0 = rospy.Time.now().to_sec()
 	current_distance = 0
 	while(current_distance < distance):
+	   print("current distance")
+	   print(current_distance)
 	   velocity_publisher.publish(vel_msg)
 	   t1 = rospy.Time.now().to_sec()
 	   current_distance = speed*(t1-t0)
-	vel_msg.linear.x = 0
+	#vel_msg.linear.x = 0
 	velocity_publisher.publish(vel_msg)
-    pub.publish(twist_output)
+    velocity_publisher.publish(twist_output)
 
 if __name__ == '__main__':
   motion_demo()
+  print("script has completed")
 
